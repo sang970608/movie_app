20b4801f7635c4c67c41cf56a80b1be092cea79d
sang970608 <sang970608@naver.com>
2020-09-01 19:35:07 +0900

    no message

---
+++ precache-manifest.6f457eee71a28dc94940de46988aa579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6846ddb86db091f3483a27cb8470e348",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "78267716c5a8e1bf0591",
+    "url": "/movie_app/static/css/main.bd508145.chunk.css"
+  },
+  {
+    "revision": "500f756bc09d96f278cf",
+    "url": "/movie_app/static/js/2.9c941358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.9c941358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78267716c5a8e1bf0591",
+    "url": "/movie_app/static/js/main.6f7cd386.chunk.js"
+  },
+  {
+    "revision": "c214cca33033549ac5cc",
+    "url": "/movie_app/static/js/runtime-main.c5127233.js"
+  }
+]);
