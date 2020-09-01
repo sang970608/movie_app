6d619247e60274eb55a0a778bb47965e39f7dbfc
sang970608 <sang970608@naver.com>
2020-09-01 19:59:00 +0900

    Updates

---
+++ precache-manifest.b406cecda7ccc8e72fd2652ee3c18f7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50004a7413800a1bd24c85c48f8a090",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e51dcdeb22d68ac73979",
+    "url": "/movie_app/static/css/main.bd508145.chunk.css"
+  },
+  {
+    "revision": "500f756bc09d96f278cf",
+    "url": "/movie_app/static/js/2.9c941358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.9c941358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51dcdeb22d68ac73979",
+    "url": "/movie_app/static/js/main.59ba9b37.chunk.js"
+  },
+  {
+    "revision": "c214cca33033549ac5cc",
+    "url": "/movie_app/static/js/runtime-main.c5127233.js"
+  }
+]);
